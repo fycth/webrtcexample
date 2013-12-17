3f1c2adeb2d71a16a05106d4d62ee12157c4d9e5
Andrey Sergienko <andrey.sergienko@gmail.com>
2013-12-17 16:32:51 +0000

    public STUN TURN added

--- www/js/WebRTCO.js
+++ www/js/WebRTCO.js
@@ -33,6 +33,44 @@ var WebRTCO = function() {
     var callbackReceiveChatMessage = null;
     var callbackOnRoomReceived = null;
 
+    var pc_config = {"iceServers":
+       [{url:'stun:23.21.150.121'},
+        {url:'stun:stun.l.google.com:19302'},
+        {url:'stun:stun01.sipphone.com'},
+        {url:'stun:stun.ekiga.net'},
+        {url:'stun:stun.fwdnet.net'},
+        {url:'stun:stun.ideasip.com'},
+        {url:'stun:stun.iptel.org'},
+        {url:'stun:stun.rixtelecom.se'},
+        {url:'stun:stun.schlund.de'},
+        {url:'stun:stun.l.google.com:19302'},
+        {url:'stun:stun1.l.google.com:19302'},
+        {url:'stun:stun2.l.google.com:19302'},
+        {url:'stun:stun3.l.google.com:19302'},
+        {url:'stun:stun4.l.google.com:19302'},
+        {url:'stun:stunserver.org'},
+        {url:'stun:stun.softjoys.com'},
+        {url:'stun:stun.voiparound.com'},
+        {url:'stun:stun.voipbuster.com'},
+        {url:'stun:stun.voipstunt.com'},
+        {url:'stun:stun.voxgratia.org'},
+        {url:'stun:stun.xten.com'},
+        {
+            url: 'turn:numb.viagenie.ca',
+            credential: 'muazkh',
+            username: 'webrtc@live.com'
+        },
+        {
+            url: 'turn:192.158.29.39:3478?transport=udp',
+            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+            username: '28224511:1379330808'
+        },
+        {
+            url: 'turn:192.158.29.39:3478?transport=tcp',
+            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+            username: '28224511:1379330808'
+        }]};
+
     window.onbeforeunload = function() {
         sendMessage({type: 'bye'});
     };
@@ -166,10 +204,10 @@ var WebRTCO = function() {
     };
 
     var createPeerConnection = function() {
-        var pc_config = {"iceServers": [{"url": "stun:stun.l.google.com:19302"}]};
+//        var pc_config = {"iceServers": [{"url": "stun:stun.l.google.com:19302"}]};
         var pc_constraints = {"optional": [{"DtlsSrtpKeyAgreement": true}]};
 
-        if (webrtcDetectedBrowser == "firefox") pc_config = {"iceServers":[{"url":"stun:23.21.150.121"}]};
+//        if (webrtcDetectedBrowser == "firefox") pc_config = {"iceServers":[{"url":"stun:23.21.150.121"}]};
 
         try {
             pc = new RTCPeerConnection(pc_config, pc_constraints);
