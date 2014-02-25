8632a15eba967dc341e06448449eed8e6ef6ebe0
fycth <andrey.sergienko@gmail.com>
2014-02-25 21:11:41 +0000

    removed signaling server from example

--- start.bat
+++
@@ -1,2 +0,0 @@
-erl -pa deps\cowboy\ebin deps\cowlib\ebin deps\goldrush\ebin deps\gproc\ebin deps\lager\ebin deps\ranch\ebin apps\signaler\ebin -sasl errlog_type error -s signaler_app -config etc\app.config
-
--- start.sh
+++
@@ -1,1 +0,0 @@
-erl -pa deps/*/ebin apps/*/ebin -sasl errlog_type error -s signaler_app -config etc/app.config
